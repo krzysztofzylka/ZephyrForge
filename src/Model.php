93a36d8450810afd8817165785205e69e78d11ad
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2024-01-27 00:09:01 +0100

    Implement `readAll` and `count` methods in Model.php
    Added `readAll` and `count` methods to Model.php that facilitate the reading of all records and counting records based on given conditions respectively. These methods provide more flexibility while manipulating data. The `readAll` method also includes parameters to control querying (i.e. limit, orderBy, groupBy). Exception handling for database related errors is also included.

--- src/Model.php
+++ src/Model.php
@@ -103,6 +103,47 @@ class Model
     }
 
     /**
+     * Reads all records from the table based on the given conditions and returns an array of results.
+     * @param array|null $conditions An array of conditions to filter the records (default: null).
+     * @param array|null $columns An array of columns to select from the table (default: null).
+     * @param string|null $orderBy The column to order the results by (default: null).
+     * @param string|null $limit The maximum number of records to return (default: null).
+     * @param string|null $groupBy The column to group the results by (default: null).
+     * @return false|array Returns an array of records if successful, false otherwise.
+     * @throws HiddenException If an error occurs while retrieving the records.
+     */
+    public function readAll(
+        ?array $conditions = null,
+        ?array $columns = null,
+        ?string $orderBy = null,
+        ?string $limit = null,
+        ?string $groupBy = null
+    ): false|array
+    {
+        if (!$_ENV['DATABASE'] && $this->useTable !== false) {
+            return false;
+        }
+
+        try {
+            return $this->tableInstance->findAll(
+                condition: $conditions,
+                columns: $columns,
+                orderBy: $orderBy,
+                limit: $limit,
+                groupBy: $groupBy
+            );
+        } catch (DatabaseManagerException $exception) {
+            Log::throwableLog($exception);
+
+            throw new HiddenException(
+                $exception->getHiddenMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+    }
+
+    /**
      * Updates a record in the table with the specified ID using the provided data.
      * @param int $id The ID of the record to update.
      * @param array $data An array containing the new data for the record.
@@ -118,6 +159,35 @@ class Model
         try {
             return $this->tableInstance->setId($id)->update(
                 data: $data
+            );
+        } catch (DatabaseManagerException $exception) {
+            Log::throwableLog($exception);
+
+            throw new HiddenException(
+                $exception->getHiddenMessage(),
+                $exception->getCode(),
+                $exception
+            );
+        }
+    }
+
+    /**
+     * Counts the number of records in the table based on the given conditions and grouping.
+     * @param array|null $conditions An array of conditions to filter the records (default: null).
+     * @param string|null $groupBy The column to group the results by (default: null).
+     * @return bool Returns true if the count was successful, false otherwise.
+     * @throws HiddenException If an error occurs while counting the records.
+     */
+    public function count(?array $conditions = null, ?string $groupBy = null): bool
+    {
+        if (!$_ENV['DATABASE'] && $this->useTable !== false) {
+            return false;
+        }
+
+        try {
+            return $this->tableInstance->findCount(
+                condition: $conditions,
+                groupBy: $groupBy
             );
         } catch (DatabaseManagerException $exception) {
             Log::throwableLog($exception);
