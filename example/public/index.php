5354858a118c1fc11afaaeaeb3ed5415f12be9e7
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2024-01-28 07:51:18 +0100

    Update error handling and introduce new Twig templates
    Enhanced error handling in the index.php to deliver more precise HTTP status messages. New 'error.twig' template has been added, providing user-friendly error messages. The Tailwind configuration and CSS files have also been updated to accommodate the new template.

--- example/public/index.php
+++ example/public/index.php
@@ -11,5 +11,27 @@ try {
     $view = new \Zephyrforge\Zephyrforge\View();
     $view->render(\Zephyrforge\Zephyrforge\Kernel::$projectPath . '/templates/template.twig', ['content' => $content]);
 } catch (Throwable $throwable) {
-    var_dump($throwable);
+    $codeMessage = match($throwable->getCode()) {
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        204 => 'No Content',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        304 => 'Not Modified',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        503 => 'Service Unavailable',
+        default => 'Unknown error',
+    };
+
+    $view = new \Zephyrforge\Zephyrforge\View();
+    $view->render(\Zephyrforge\Zephyrforge\Kernel::$projectPath . '/templates/error.twig', ['message' => $codeMessage, 'code' => $throwable->getCode()]);
 }
--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.js}",
     './src/**/*.twig',
+    './templates/**/*.twig',
   ],
   theme: {
     extend: {
